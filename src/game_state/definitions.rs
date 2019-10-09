d8db8b2297d997f6ff0463a826182f1d9f4ea15f
pieterjoubert <pieter.joubert@gmail.com>
2019-10-09 13:42:27 +0200

    Used Builder Pattern with Room and updated tests accordinly

--- src/game_state/definitions.rs
+++ src/game_state/definitions.rs
@@ -36,8 +36,8 @@ pub struct Room {
     pub width: f64,  //x-axis
     pub depth: f64,  //z-axis
     pub height: f64, //y-axis
-    pub textures: Vec<Material>,
-    pub lights: Vec<Light>,
+    pub textures: Option<Vec<Material>>,
+    pub lights: Option<Vec<Light>>,
 }
 
 impl Room {
@@ -46,8 +46,6 @@ impl Room {
         width: f64,
         depth: f64,
         height: f64,
-        textures: Vec<Material>,
-        lights: Vec<Light>,
     ) -> Room {
         Room {
             id: Uuid::new_v4(),
@@ -55,10 +53,21 @@ impl Room {
             width,
             depth,
             height,
-            textures,
-            lights,
+            textures : None,
+            lights : None,
         }
     }
+
+    pub fn with_textures(&mut self, textures: Option<Vec<Material>>) -> &mut Room {
+        self.textures = textures;
+            self
+    }
+
+    pub fn with_lights(&mut self, lights: Option<Vec<Light>>) -> &mut Room {
+        self.lights = lights;
+        self
+    }
+
 }
 
 
@@ -71,8 +80,8 @@ pub struct Light {
 
 impl Light {
     pub fn new(
-        pos: Vector3<f64>,
-        rot: Vector4<f64>,
+        pos: Vector3<f64>, //relative to room position
+        rot: Vector4<f64>, //absolute rotation
         light_shader: LightShader,
     ) -> Light {
         Light {
@@ -97,20 +106,19 @@ mod test {
 
     #[test]
     fn test_room_constructor() {
-        let room = Room::new(
+        let mut room = Room::new(
             Vector3::new(0.0, 0.0, 0.0),
             10.0,
             10.0,
             2.0,
-            vec![Material::new(Texture::GreyWall)],
-            vec![Light::new(Texture::GreyWall)],
         );
+        room.with_textures(Some(vec![Material::new(Texture::GreyWall)]));
         assert_eq!(room.id.is_nil(), false);
         assert_eq!(room.corner_pos.magnitude() == 0.0, true);
         assert_eq!(room.width == 10.0, true);
         assert_eq!(room.depth == 10.0, true);
         assert_eq!(room.height == 2.0, true);
-        assert_eq!(room.textures[0].texture == Texture::GreyWall, true);
-        assert_eq!(room.lights[0] == Texture::GreyWall, true);
+        assert_eq!(room.textures.unwrap()[0].texture == Texture::GreyWall, true);
+        //assert_eq!(room.lights[0] == Texture::GreyWall, true);
     }
 }
